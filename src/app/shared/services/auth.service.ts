a5bf79ece12ba0d3ea17835e961f90d92ab4ca4b
kulabuha <kulabuha@inbox.ru>
2020-03-03 21:24:08 +0300

    add token storage

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -10,23 +10,38 @@ export class AuthService {
   constructor(private  http: HttpClient) {}
 
   get token(): string {
-    return '';
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
+    user.returnSecureToken = true;
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
         tap(this.setToken)
       );
   }
 
-  logout() {}
+  logout() {
+    this.setToken(null);
+  }
 
-  isAuthenticated() {
+  isAuthenticated(): boolean {
     return !!this.token;
   }
 
   private setToken(response: FbAuthResponse) {
-    console.log(response);
+    if (response) {
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
+
   }
 }
